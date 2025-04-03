13ca28866899f587538f21eaaaa65590dc1639ac
AdrianBanulsA <adrianb_arias@uma.es>
2025-04-03 13:10:13 +0200

    Fixed install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
 # Modify PYTHONPATH for adding the 'rl_spin_decoupler' dependency
-export PYTHONPATH="/home/adrian/devel/rl_coppelia/dependencies/rl_spin_decoupler:$PYTHONPATH"
+BASE_DIR="$(pwd)"
+DEPS_DIR="$BASE_DIR/dependencies/rl_spin_decoupler"
+export PYTHONPATH="$DEPS_DIR:$PYTHONPATH"
+echo "PYTHONPATH set to: $PYTHONPATH"
 
 # Install all the required packages (including 'rl_coppelia' package in editable mode
 pip install -r requirements.txt
